8388db750514e89d686432bd240cead5c505f5cd
formacion <formacion@192.168.176.135>
2018-03-21 11:09:51 +0100

    completo

--- src/main/java/es/fpdual/eadmin/eadmin/modelo/DocumentoRegistro.java
+++ src/main/java/es/fpdual/eadmin/eadmin/modelo/DocumentoRegistro.java
@@ -5,14 +5,22 @@ import java.util.Date;
 public class DocumentoRegistro extends Documento {
 	private String dniInteresado;
 	private String codigoRegistro;
+
 	public DocumentoRegistro(Integer codigo, String nombre, Date fechaCreacion, Boolean publico,
 			EstadoDocumento estado) {
 		super(codigo, nombre, fechaCreacion, publico, estado);
-		
+
 		this.dniInteresado = dniInteresado;
 		this.codigoRegistro = codigoRegistro;
-		
-		
+
+	}
+
+	public String getDniInteresado() {
+		return dniInteresado;
+	}
+
+	public String getCodigoRegistro() {
+		return codigoRegistro;
 	}
 
 }
