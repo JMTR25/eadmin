9e49c6c139b3afd6f066ccbf786020d21762d399
formacion <formacion@192.168.176.135>
2018-03-21 10:10:31 +0100

    ..

--- src/main/java/es/fpdual/eadmin/eadmin/modelo/DocumentoRegistro.java
+++ src/main/java/es/fpdual/eadmin/eadmin/modelo/DocumentoRegistro.java
@@ -1,5 +1,18 @@
 package es.fpdual.eadmin.eadmin.modelo;
 
-public class DocumentoRegistro {
+import java.util.Date;
+
+public class DocumentoRegistro extends Documento {
+	private String dniInteresado;
+	private String codigoRegistro;
+	public DocumentoRegistro(Integer codigo, String nombre, Date fechaCreacion, Boolean publico,
+			EstadoDocumento estado) {
+		super(codigo, nombre, fechaCreacion, publico, estado);
+		
+		this.dniInteresado = dniInteresado;
+		this.codigoRegistro = codigoRegistro;
+		
+		
+	}
 
 }
